371bf1eaaaef42ad5512f4dc52bd1cbd28f402ee
nex3z <litianxing9@gmail.com>
2015-10-10 21:43:00 +0800

    Add API key for OWM.

--- app/src/main/java/com/nex3z/sunshine/ForecastFragment.java
+++ app/src/main/java/com/nex3z/sunshine/ForecastFragment.java
@@ -98,7 +98,8 @@ public class ForecastFragment extends Fragment {
                 // Construct the URL for the OpenWeatherMap query
                 // Possible parameters are avaiable at OWM's forecast API page, at
                 // http://openweathermap.org/API#forecast
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7"
+                        + "&APPID=" + getString(R.string.api_key_owm));
 
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
